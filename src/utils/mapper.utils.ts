92825889ba666db071da0874cfa213ab0d744a60
LewieJ08 <lewiej08@gmail.com>
2026-03-23 10:05:35 +0000

    Add commit mapper util

--- src/utils/mapper.utils.ts
+++ src/utils/mapper.utils.ts
@@ -1,3 +1,4 @@
+import { Commit, CommitDTO } from "../types/commit.types";
 import { 
     BranchRef,
     BranchRefDTO,
@@ -69,3 +70,27 @@ export function mapPullRequest(dto: PullRequestDTO): PullRequest {
 export function mapPullRequests(dtos: PullRequestDTO[]): PullRequest[] {
     return dtos.map((dto) => mapPullRequest(dto))
 }
+
+export function mapCommit(dto: CommitDTO): Commit {
+    return {
+        sha: dto.sha,
+        nodeId: dto.node_id,
+        message: dto.commit.message,
+        author: {
+            name: dto.commit.author.name,
+            email: dto.commit.author.email,
+            date: new Date(dto.commit.author.date),
+            user: dto.author ? mapUser(dto.author) : null
+        },
+        committer: {
+            name: dto.commit.committer.name,
+            email: dto.commit.committer.email,
+            date: new Date(dto.commit.committer.date),
+            user: dto.committer ? mapUser(dto.committer) : null
+        },
+        url: dto.html_url,
+        commentCount: dto.commit.comment_count,
+        verified: dto.commit.verification.verified,
+        parentShas: dto.parents.map(parent => parent.sha)
+    }
+} 
