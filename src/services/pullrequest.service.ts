d3ef26703a4f6021a0f79b176e22382a5fb3f265
LewieJ08 <lewiej08@gmail.com>
2026-04-03 00:34:56 +0100

    Document PullRequestService via TSDoc

--- src/services/pullrequest.service.ts
+++ src/services/pullrequest.service.ts
@@ -16,6 +16,34 @@ import {
 } from "../mappers/pullrequest.mapper";
 import { mapCommits } from "../mappers/commit.mapper";
 
+interface CreatePullRequestParams {
+    head: string;
+    base: string;
+    title?: string;
+    headRepo?: string;
+    body?: string;
+    maintainerCanModify?: boolean;
+    draft?: boolean;
+    issue?: number;
+}
+
+interface UpdatePullRequestParams {
+    pullNumber: number,
+    title?: string,
+    body?: string,
+    state?: PullRequestState,
+    base?: string,
+    maintainerCanModify?: boolean
+}
+
+interface MergePullRequestParams {
+    pullNumber: number,
+    commitTitle?: string,
+    commitMessage?: string,
+    sha?: string,
+    mergeMethod?: PullRequestMergeMethod    
+}
+
 export class PullRequestService {
     private readonly path: string;
 
@@ -27,21 +55,53 @@ export class PullRequestService {
         this.path = `/repos/${this.client.config.owner}/${this.client.config.repo}/pulls`;
     }
 
+    /**
+     * List pull requests of a repository 
+     * @returns Array of pull requests 
+     * 
+     * @example
+     * ```ts 
+     * const pullRequests = await github.pullRequests.list();
+     * ```
+     */
     public async list(): Promise<PullRequest[]> {
         const response = await this.client.request<PullRequestDTO[]>(this.path);
         return mapPullRequests(response.data)
     }
 
-    public create(
-        head: string,
-        base: string,
-        title?: string,
-        headRepo?: string,
-        body?: string,
-        maintainerCanModify?: boolean,
-        draft?: boolean,
-        issue?: number,
-    ) {
+    /**
+     * Create a pull request
+     * @param params Create pull request params object
+     * @param params.title The title of the new pull request. Required unless issue is specified
+     * @param params.head The name of the branch where your changes are implemented
+     * @param params.headRepo The name of the repository where the changes in the pull request were made 
+     * @param params.base The name of the branch you want the changes pulled into
+     * @param params.body The contents of the pull request
+     * @param params.maintainerCanModify Indicates whether maintainers can modify the pull request
+     * @param params.draft Indicates whether the pull request is a draft
+     * @param params.issue An issue in the repository to convet to a pull request
+     * 
+     * @example 
+     * ```ts
+     * await github.pullRequests.create({
+     *     title: 'Cool new feature',
+     *     body: 'This feature is very helpful and needs pulling',
+     *     head: 'LewieJ08:dev',
+     *     base: 'main' 
+     * });
+     * ```
+     */
+    public create(params: CreatePullRequestParams) {
+        const {
+            title,
+            head,
+            base,
+            headRepo,
+            body,
+            maintainerCanModify,
+            draft,
+            issue
+        } = params;
         return this.client.request(this.path, {
             method: 'POST',
             body: JSON.stringify({
@@ -57,19 +117,51 @@ export class PullRequestService {
         })
     }
 
+    /**
+     * List details of a pull request by providing its number
+     * @param pullNumber The number that identifies the pull request
+     * @returns Data of a pull request
+     * 
+     * @example
+     * ```ts
+     * const pullRequest = await github.pullRequests.get(8);
+     * ```
+     */
     public async get(pullNumber: number): Promise<PullRequest> {
         const response = await this.client.request<PullRequestDTO>(`${this.path}/${pullNumber}`);
         return mapPullRequest(response.data);
     }
 
-    public update(
-        pullNumber: number,
-        title?: string,
-        body?: string,
-        state?: PullRequestState,
-        base?: string,
-        maintainerCanModify?: boolean
-    ) {
+    /**
+     * Update a pull request 
+     * @param params Update pull request params object
+     * @param params.pullNumber The number that identifies the pull request
+     * @param params.title The title of the pull request
+     * @param params.body The contents of the pull request
+     * @param params.state State of this pull request. Either 'open' or 'closed'
+     * @param params.base The name of the branch you want your changes pulled into
+     * @param params.maintainerCanModify Indicates whether maintainers can modify the pull request
+     * 
+     * @example
+     * ```ts
+     * await github.pullRequests.update({
+     *     pullNumber: 8,
+     *     title: 'new title',
+     *     body: 'updated body',
+     *     state: 'open'
+     *     base: 'main'
+     * });
+     * ```
+     */
+    public update(params: UpdatePullRequestParams) {
+        const {
+            pullNumber,
+            title,
+            body,
+            state,
+            base,
+            maintainerCanModify
+        } = params
         return this.client.request(`${this.path}/${pullNumber}`, {
             method: 'PATCH',
             body: JSON.stringify({
@@ -82,16 +174,46 @@ export class PullRequestService {
         });
     } 
     
+    /**
+     * List commits on a pull request
+     * @param pullNumber The number that identifies the pull request
+     * @returns Array of commits 
+     * 
+     * @example 
+     * ```ts 
+     * const commits = await github.pullRequests.listCommits(8);
+     * ```
+     */
     public async listCommits(pullNumber: number): Promise<Commit[]> {
         const response = await this.client.request<CommitDTO[]>(`${this.path}/${pullNumber}/commits`);
         return mapCommits(response.data);
     }
 
+    /**
+     * List the files in a specified pull request
+     * @param pullNumber The number that identifies the pull request
+     * @returns Array of pull request files 
+     * 
+     * @example 
+     * ```ts 
+     * const pullRequestFiles = await github.pullRequests.listFiles(8);
+     * ```
+     */
     public async listFiles(pullNumber: number): Promise<PullRequestFile[]> {
         const response = await this.client.request<PullRequestFileDTO[]>(`${this.path}/${pullNumber}/files`);
         return mapPullRequestFiles(response.data)
     }
  
+    /**
+     * Check if a pull request has been merged
+     * @param pullNumber The number that identifies the pull request
+     * @returns boolean value that determines if the pull request is merged
+     * 
+     * @example
+     * ```ts 
+     * const isMerged = await github.pullRequests.isMerged(8);
+     * ``` 
+     */
     public async isMerged(pullNumber: number): Promise<boolean> {
         const response = await this.client.request<null>(`${this.path}/${pullNumber}/merge`);
 
@@ -102,13 +224,32 @@ export class PullRequestService {
         return false
     }
 
-    public merge(
-        pullNumber: number,
-        commitTitle?: string,
-        commitMessage?: string,
-        sha?: string,
-        mergeMethod?: PullRequestMergeMethod
-    ) {
+    /**
+     * Merge a pull request into the base branch
+     * @param params Merge pull request params object 
+     * @param params.pullNumber The number that identifies the pull requests
+     * @param params.commitTitle Title for the automatic commit message 
+     * @param params.commitMessage Extra Detail to append the automatic commit message
+     * @param params.sha SHA that pull request head must match to allow merge
+     * @param params.mergeMethod The merge method to use. 'merge', 'squash' or 'rebase'
+     * 
+     * @example
+     * ```ts 
+     * await github.pullRequests.merge({
+     *     pullNumber: 8,
+     *     commitTitle: 'Expand docs',
+     *     commitMessage: 'Add docs for new methods'
+     * });
+     * ```
+     */
+    public merge(params: MergePullRequestParams) {
+        const {
+            pullNumber,
+            commitTitle,
+            commitMessage,
+            sha,
+            mergeMethod
+        } = params
         return this.client.request(`${this.path}/${pullNumber}/merge`, {
             method: 'PUT',
             body: JSON.stringify({
@@ -120,6 +261,16 @@ export class PullRequestService {
         });
     }
 
+    /**
+     * Updates the pull request with the latest upstream changes 
+     * @param pullNumber The number that identifies the pull request
+     * @param expectedHeadSha The expected SHA of the pull requests HEAD ref. This is the most recent commit of the pull request's branch
+     *  
+     * @example 
+     * ```ts  
+     * await github.pullRequests.updateBranch(8, 6dcb09b5b57875f334f61aebed695e2e4193db5e)
+     * ```
+     */
     public updateBranch(pullNumber: number, expectedHeadSha?: string) {
         return this.client.request(`${this.path}/${pullNumber}/update-branch`, {
             method: 'PUT',
