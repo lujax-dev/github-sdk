9160a0740830445edc30da6612d4cfd886389e57
LewieJ08 <lewiej08@gmail.com>
2026-05-08 17:06:22 +0100

    Add methods to RepositoryService class

--- src/mappers/repository.mapper.ts
+++ src/mappers/repository.mapper.ts
@@ -8,7 +8,17 @@ import {
     RepositoryActivityDTO, 
     RepositoryActivity, 
     RepositoryActivityTypeDTO,
-    RepositoryActivityType
+    RepositoryActivityType,
+    ImmutableReleasesStatusDTO,
+    ImmutableReleasesStatus,
+    RepositoryTag,
+    RepositoryTagDTO,
+    TeamDTO,
+    Team,
+    TeamParent,
+    TeamParentDTO,
+    TeamPermissionsDTO,
+    TeamPermissions
 } from "../types/repository.types";
 import { mapUser } from "./user.mapper";
 
@@ -75,6 +85,97 @@ export function mapRepositoryActivity(dto: RepositoryActivityDTO): RepositoryAct
 
 export function mapRepositoryActivities(dtos: RepositoryActivityDTO[]): RepositoryActivity[] {
     return dtos.map(dto => mapRepositoryActivity(dto));
+}
+
+export function mapImmutableReleasesStatus(dto: ImmutableReleasesStatusDTO): ImmutableReleasesStatus {
+    return {
+        enabled: dto.enabled,
+        enforcedByOwner: dto.enforced_by_owner
+    }
+}
+
+export function mapRepositoryTag(dto: RepositoryTagDTO): RepositoryTag {
+    return {
+        name: dto.name,
+        nodeId: dto.node_id,
+        zipballUrl: dto.zipball_url,
+        tarballUrl: dto.tarball_url,
+        commit: dto.commit
+    }
+}
+
+export function mapRepositoryTags(dtos: RepositoryTagDTO[]): RepositoryTag[] {
+    return dtos.map(dto => mapRepositoryTag(dto));
+}
+
+export function mapTeamPermissions(dto: TeamPermissionsDTO): TeamPermissions {
+    return {
+        pull: dto.pull,
+        triage: dto.triage,
+        push: dto.push,
+        maintain: dto.maintain,
+        admin: dto.admin,
+    };
+}
+
+export function mapTeamParent(dto: TeamParentDTO): TeamParent {
+    return {
+        id: dto.id,
+        nodeId: dto.node_id,
+        url: dto.url,
+        membersUrl: dto.members_url,
+        name: dto.name,
+        description: dto.description,
+        permission: dto.permission,
+        privacy: dto.privacy,
+        notificationSetting: dto.notification_setting,
+        htmlUrl: dto.html_url,
+        repositoriesUrl: dto.repositories_url,
+        slug: dto.slug,
+        ldapDn: dto.ldap_dn,
+        type: dto.type,
+        organizationId: dto.organization_id,
+        enterpriseId: dto.enterprise_id,
+    };
+}
+
+export function mapTeam(dto: TeamDTO): Team {
+    return {
+        id: dto.id,
+        nodeId: dto.node_id,
+
+        name: dto.name,
+        slug: dto.slug,
+
+        description: dto.description,
+
+        privacy: dto.privacy,
+        notificationSetting: dto.notification_setting,
+
+        permission: dto.permission,
+
+        permissions: dto.permissions
+        ? mapTeamPermissions(dto.permissions)
+        : undefined,
+
+        url: dto.url,
+        htmlUrl: dto.html_url,
+        membersUrl: dto.members_url,
+        repositoriesUrl: dto.repositories_url,
+
+        type: dto.type,
+
+        organizationId: dto.organization_id,
+        enterpriseId: dto.enterprise_id,
+
+        parent: dto.parent
+        ? mapTeamParent(dto.parent)
+        : null,
+    };
+}
+
+export function mapTeams(dtos: TeamDTO[],): Team[] {
+    return dtos.map(dto => mapTeam(dto));
 }
 
 export function mapCreateRepositoryParams(params: CreateRepositoryParams): CreateRepositoryPayload {
--- src/services/repository.service.ts
+++ src/services/repository.service.ts
@@ -1,11 +1,35 @@
 import { GithubClient } from "../client/GithubClient";
-import { mapCreateRepositoryParams, mapRepositories, mapRepository, mapUpdateRepositoryParams } from "../mappers/repository.mapper";
+import { 
+    mapCreateRepositoryParams,
+    mapImmutableReleasesStatus,
+    mapRepositories, 
+    mapRepository, 
+    mapRepositoryActivities, 
+    mapRepositoryTags, 
+    mapTeams, 
+    mapUpdateRepositoryParams 
+} from "../mappers/repository.mapper";
+import { mapContributers} from "../mappers/user.mapper";
 import { 
     Repository, 
     RepositoryDTO,
     CreateRepositoryParams, 
-    UpdateRepositoryParams
+    UpdateRepositoryParams,
+    RepositoryActivityDTO,
+    RepositoryActivity,
+    DependabotSecurityUpdatesStatus,
+    CodeownersError,
+    CodeownersErrorsResponse,
+    ImmutableReleasesStatus,
+    ImmutableReleasesStatusDTO,
+    RepositoryLanguages,
+    Status,
+    RepositoryTag,
+    RepositoryTagDTO,
+    Team,
+    TeamDTO
 } from "../types/repository.types";
+import { Contributer, ContributerDTO } from "../types/user.types";
 import { assertConfig } from "../utils/config.utils";
 
 export class RepositoryService {
@@ -58,7 +82,7 @@ export class RepositoryService {
     }
 
     /**
-     * Get a repository
+     * Get the configured repository
      * 
      * @returns Data of the repository
      * 
@@ -74,7 +98,7 @@ export class RepositoryService {
     }
 
     /**
-     * Update a repository
+     * Update the configured repository
      * 
      * @param params Configuration for the repository to update
      * @returns Data of the updated repository
@@ -82,7 +106,6 @@ export class RepositoryService {
      * @example 
      * ```ts
      * github.repositories.update({
-     *     pullNumber: 8,
      *     name: 'new-repo-name',
      *     description: 'this description is now better'
      * });
@@ -96,5 +119,297 @@ export class RepositoryService {
             body: JSON.stringify(body)
         });
         return mapRepository(response.data);
+    }
+
+    /**
+     * Delete the configured repository
+     * 
+     * @returns Boolean value that determines if repo was deleted
+     * 
+     * @example 
+     * ```ts 
+     * github.repositories.delete();
+     * ```
+     */
+    public async delete(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(this.path, {
+            method: 'DELETE'
+        });
+        return response.status === 204;
+    }   
+    
+    /**
+     * List activities of the configured repository
+     * 
+     * @returns Array of repository activities
+     * 
+     * @example 
+     * ```ts
+     * const activities = await github.repositories.listActivities();
+     * ```
+     */
+    public async listActivities(): Promise<RepositoryActivity[]> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<RepositoryActivityDTO[]>(`${this.path}/activity`);
+        return mapRepositoryActivities(response.data);
+    }
+
+    /**
+     * Get the Dependabot security updates status for the configured repository
+     * 
+     * @returns The current Dependabot security updates status
+     * 
+     * @example 
+     * ```ts
+     * const securityUpdatesEnabled = await github.repositories.getDependabotSecurityUpdatesStatus();
+     * ```
+     */
+    public async getDependabotSecurityUpdatesStatus(): Promise<DependabotSecurityUpdatesStatus> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<DependabotSecurityUpdatesStatus>(`${this.path}/automated-security-fixes`);
+        return response.data
+    }   
+    
+    /**
+     * Enable Dependabot security updates for the configured repository
+     * 
+     * @returns Boolean value that determines if security updates were enabled
+     * 
+     * @example
+     * ```ts
+     * github.repositories.enableDependabotSecurityUpdates();
+     * ```
+     */
+    public async enableDependabotSecurityUpdates(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(`${this.path}/automated-security-fixes`, {
+            method: 'PUT'
+        });
+        return response.status === 204
+    }
+
+    /**
+     * Disable Dependabot security updates for the configured repository
+     * 
+     * @returns Boolean value that determines if security updates were disabled
+     * 
+     * @example 
+     * ```ts
+     * github.repositories.disableDependabotSecurityUpdates();
+     * ```
+     */
+    public async disableDependabotSecurityUpdates(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(`${this.path}/automated-security-fixes`, {
+            method: 'DELETE'
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * List any syntax errors that are detected in the CODEOWNERS file,
+     * within the configured repository
+     * 
+     * @returns Array of CODEOWNERS errors
+     * 
+     * @example
+     * ```ts
+     * const codeownersErrors = await github.repositories.getCodeownersErrors();
+     * ```
+     */
+    public async getCodeownersErrors(): Promise<CodeownersError[]> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<CodeownersErrorsResponse>(`${this.path}/codeowners/errors`);
+        return response.data.errors;
+    }
+
+    /**
+     * Get all contributers of the configured repository
+     * 
+     * @returns Array of contributors
+     * 
+     * @example 
+     * ```ts
+     * const contributers = await github.repositories.getContributers();
+     * ```
+     */
+    public async getContributers(): Promise<Contributer[]> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<ContributerDTO[]>(`${this.path}/contributors`);
+        return mapContributers(response.data);
+    }
+
+    /**
+     * Create a dispatch event for the configured repository
+     * 
+     * @param eventType A custom webhook event name. Must be 100 characters or less
+     * @returns Boolean value that determines if dispatch event was created
+     * 
+     * @example
+     * ```ts
+     * github.repositories.createDispatchEvent('on-demand-test');
+     * ```
+     */
+    public async createDispatchEvent(eventType: string): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(`${this.path}/dispatches`, {
+            method: 'POST',
+            body: JSON.stringify({ event_type: eventType })
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * Check if immutable releases are enabled for the configured repository
+     * 
+     * @returns The current immutable releases status
+     * 
+     * @example 
+     * ```ts
+     * const immutableReleasesStatus = github.repositories.getImmutableReleasesStatus();
+     * ```
+     */
+    public async getImmutableReleasesStatus(): Promise<ImmutableReleasesStatus> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<ImmutableReleasesStatusDTO>(`${this.path}/immutable-releases`);
+        return mapImmutableReleasesStatus(response.data);
+    }
+
+    /**
+     * Enable immutable releases for the configured
+     * 
+     * @returns Boolean value that determines if immutable releases were enabled
+     * 
+     * @example
+     * ```ts
+     * github.repositories.enableImmutableReleases();
+     * ```
+     */
+    public async enableImmutableReleases(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(`${this.path}/immutable-releases`, {
+            method: 'PUT'
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * Disable immutable releases for the configured
+     * 
+     * @returns Boolean value that determines if immutable releases were disabled
+     * 
+     * @example
+     * ```ts
+     * github.repositories.disableImmutableReleases();
+     * ```
+     */
+    public async disableImmutableReleases(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<null>(`${this.path}/immutable-releases`, {
+            method: 'DELETE'
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * List languages for the configured repository
+     * 
+     * @returns Object containing all languages 
+     * 
+     * @example 
+     * ```ts
+     * const languages = github.repositories.listLanguages();
+     * ```
+     */
+    public async listLanguages(): Promise<RepositoryLanguages> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<RepositoryLanguages>(`${this.path}/languages`);
+        return response.data;
+    }
+
+    /**
+     * Check if private vulnerability reporting is enabled
+     * for the configured repository
+     * 
+     * @returns Boolean that determines if private vulnerability reporting is enabled
+     * 
+     * @example
+     * ```ts
+     * const privateVulnerabilityReporting = await github.repositories.getPrivateVulnerabilityReportingStatus();
+     * ```
+     */
+    public async getPrivateVulnerabilityReportingStatus(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<Status>(`${this.path}/private-vulnerability-reporting`);
+        return response.data.enabled === true
+    }
+
+    /**
+     * Enable private vulnerability reporting for the configured repository
+     * 
+     * @returns Boolean that determines if private vulnerability reporting 
+     * 
+     * @example 
+     * ```ts
+     * github.repositories.enablePrivateVulnerabilityReporting();
+     * ```
+     */
+    public async enablePrivateVulnerabilityReporting(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request(`${this.path}/private-vulnerability-reporting`, {
+            method: 'PUT'
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * Disable private vulnerability reporting for the configured repository
+     * 
+     * @returns Boolean that determines if private vulnerability reporting 
+     * 
+     * @example 
+     * ```ts
+     * github.repositories.disablePrivateVulnerabilityReporting();
+     * ```
+     */
+    public async disablePrivateVulnerabilityReporting(): Promise<boolean> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request(`${this.path}/private-vulnerability-reporting`, {
+            method: 'DELETE'
+        });
+        return response.status === 204;
+    }
+
+    /**
+     * List all tags of the configured repository
+     * 
+     * @returns Array of all repository tags
+     * 
+     * @example
+     * ```ts 
+     * const repositoryTags = await github.repositories.listTags();
+     * ```
+     */
+    public async listTags(): Promise<RepositoryTag[]> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<RepositoryTagDTO[]>(`${this.path}/tags`);
+        return mapRepositoryTags(response.data);
+    }
+
+    /**
+     * List all teams with access to the configured repository
+     * 
+     * @returns Array of teams
+     * 
+     * @example
+     * ```ts
+     * const teams = github.repositories.listTeams();
+     * ```
+     */
+    public async listTeams(): Promise<Team[]> {
+        assertConfig(this.client, ['owner', 'repo']);
+        const response = await this.client.request<TeamDTO[]>(`${this.path}/teams`);
+        return mapTeams(response.data);
     }
 }
